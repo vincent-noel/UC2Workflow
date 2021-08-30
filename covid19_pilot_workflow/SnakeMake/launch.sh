60e9387aeb0234e0661d4f52735fcef1a1cb53ea
hnortamo <nortamoh@puhti-login1.bullx>
2021-08-30 17:10:50 +0300

    Working local snakemake example

--- covid19_pilot_workflow/SnakeMake/launch.sh
+++ covid19_pilot_workflow/SnakeMake/launch.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-
-permedcoe execute application Snakefile --workflow_manager snakemake --flags "--cores 1"
-
-# Using shortcuts:
-# permedcoe x app Snakefile -w snakemake -f "--cores 1"
+export COVID19_BB_IMAGES=$(readlink -f ../../resources/images/)/
+export COVID19_BB_ASSETS=$(readlink -f ../../resources/assets/)/
+export _DATA_DIR=$(readlink -f ../../resources/data/)/
+export SINGULARITY_BIND="$COVID19_BB_ASSETS:$COVID19_BB_ASSETS,$_DATA_DIR:$_DATA_DIR"
+snakemake --cores 9 sample_aggregate
