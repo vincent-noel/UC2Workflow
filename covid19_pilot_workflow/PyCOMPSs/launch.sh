d8de7670309d7d0d556ec4a9b4b90b4c925b8932
fconejer <francisco.conejero@bsc.es>
2021-11-12 11:33:03 +0100

    Updated to COMPSs 2.10.pr

--- covid19_pilot_workflow/PyCOMPSs/launch.sh
+++ covid19_pilot_workflow/PyCOMPSs/launch.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
 export COMPSS_PYTHON_VERSION=3-ML
-#module load COMPSs/2.9
-module use /apps/modules/modulefiles/tools/COMPSs/.custom
-module load TrunkJCB
+module load COMPSs/2.10.pr
 module load singularity/3.5.2
 module use /apps/modules/modulefiles/tools/COMPSs/libraries
 module load permedcoe  # generic permedcoe package
@@ -15,14 +13,14 @@ dataset=${COVID19_PILOT_DATASET}               # Currently using the "permedcoe"
 
 enqueue_compss \
     --num_nodes=2 \
-    --exec_time=15 \
+    --exec_time=45 \
     --worker_working_dir=$(pwd) \
     --log_level=off \
     --graph \
     --tracing \
     --python_interpreter=python3 \
     covid19_pilot.py \
-        ${dataset}metadata_small.tsv \
+        ${dataset}metadata_clean.tsv \
         ${dataset}epithelial_cell_2 \
         $(pwd)/results/ \
         $(pwd)/ko_file.txt \
